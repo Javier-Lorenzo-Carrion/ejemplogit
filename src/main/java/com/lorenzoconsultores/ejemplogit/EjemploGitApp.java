0e7770a5c2b9056ba26b1b69fb7bfe460b568626
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-08-25 19:33:47 +0100

    change message to chinese

--- src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
+++ src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
@@ -1,6 +1,6 @@
 package com.lorenzoconsultores.ejemplogit;
 public class EjemploGitApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("你好世界");
     }
 }
