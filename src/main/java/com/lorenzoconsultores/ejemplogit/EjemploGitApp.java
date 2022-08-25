274420430715185465ff2a575f4087b94dfa7f6f
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-08-25 19:53:52 +0100

    change message to Spanish language

--- src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
+++ src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
@@ -1,6 +1,6 @@
 package com.lorenzoconsultores.ejemplogit;
 public class EjemploGitApp {
     public static void main(String[] args) {
-        System.out.println("こんにちは世界");
+        System.out.println("Hola mundo");
     }
 }
