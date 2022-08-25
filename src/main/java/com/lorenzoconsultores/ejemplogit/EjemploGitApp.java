358b9b078c0ad8020bb48818b086c9653238abbb
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2022-08-25 19:32:40 +0100

    change language message

--- src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
+++ src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
@@ -1,6 +1,6 @@
 package com.lorenzoconsultores.ejemplogit;
 public class EjemploGitApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("こんにちは世界");
     }
 }
